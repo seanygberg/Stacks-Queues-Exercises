861d277c0d88e9448eed3f2272a2ad7f6650041e
seanygberg <60160094+seanygberg@users.noreply.github.com>
2024-10-17 13:58:22 -0400

    first commit

---
+++ queue.js
@@ -0,0 +1,66 @@
+/** Node: node for a queue. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+
+  enqueue(val) {
+    const node = new Node(val);
+    if (this.isEmpty()) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    this.size++;
+  }
+
+  /** dequeue(): remove the node from the start of the queue
+   * and return its value. Should throw an error if the queue is empty. */
+
+  dequeue() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty");
+    }
+    const result = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    if (this.size === 0) {
+      this.last = null;
+    }
+    return result;
+  }
+
+  /** peek(): return the value of the first node in the queue. */
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty");
+    }
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the queue is empty, otherwise false */
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Queue;
